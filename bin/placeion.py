4ccf4b4f410850bd0e43ecc90dd3b8970a351c58
Oliver Beckstein <orbeckst@gmail.com>
2010-11-04 19:13:43 -0400

    darthtater: only 1 thread

--- bin/placeion.py
+++ bin/placeion.py
@@ -103,13 +103,15 @@ apbs %(infile)s > %(outfile)s
   'darthtater':
 """#$ -N %(jobname)s
 #$ -S /bin/bash
-#$ -l mem_free=500M,mem_total=500M
+#$ -l mem_free=600M
 #$ -cwd
 #$ -j y
 #$ -r y
 
 echo "APBS Born profile job running on $HOSTNAME"
 ulimit -c 64
+echo "ensuring single threaded calculation"
+export OMP_NUM_THREADS=1
 /share/apps/APBS/1.3.0/bin/apbs %(infile)s > %(outfile)s
 
 """,
