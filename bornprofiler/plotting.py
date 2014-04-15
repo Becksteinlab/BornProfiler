ebdb6882e28239eeb98fa631c952aa3cd10776eb
Lennard van der Feltz <lvanderf@asu.edu>
2014-04-15 14:53:40 -0700

    Added protein and membrane plotting, fixed minor typos

--- bornprofiler/plotting.py
+++ bornprofiler/plotting.py
@@ -25,7 +25,7 @@ from bornprofiler.config import cfg
 logger = logging.getLogger("bornprofiler")
 
 
-def graph_mult_data(file_list,xcolumn,ycolumn,plot_labels,x_label,ylabel,title):
+def graph_mult_data(file_list,xcolumn,ycolumn,plot_labels,x_label,y_label,title):
     logger.info("unpacking data")
     datalist = [[numpy.loadtxt(filename),label] for filename,label in zip(file_list, plot_labels)]
     logger.info("plotting")
@@ -33,12 +33,12 @@ def graph_mult_data(file_list,xcolumn,ycolumn,plot_labels,x_label,ylabel,title):
         plt.plot( data[:,2],data[:,3],label=label)
     plt.legend()
     plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     logger.info("saving figure to {title}.png".format(title=title))
     plt.savefig(title)
 
-def graph_mult_data_colors(file_list,xcolumn,ycolumn,plot_labels,colors,x_label,ylabel,title):
+def graph_mult_data_colors(file_list,xcolumn,ycolumn,plot_labels,colors,x_label,y_label,title):
     logger.info("unpacking data")
     datalist = [[numpy.loadtxt(filename),label,color] for filename,label,color in zip(file_list, plot_labels,colors)]
     logger.info("plotting")
@@ -46,33 +46,58 @@ def graph_mult_data_colors(file_list,xcolumn,ycolumn,plot_labels,colors,x_label,
         plt.plot( data[:,xcolumn],data[:,ycolumn],label=label,color=color)
     plt.legend()
     plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     logger.info("saving figure to {title}.png".format(title=title))
     plt.savefig(title)
 
-def graph_mult_data_cfgs(file_list,xcolumn,ycolumn,plot_labels,x_label,ylabel,title,cfgs):
+def graph_mult_data_cfgs(file_list,xcolumn,ycolumn,plot_labels,x_label,y_label,title,cfgs):
     logger.info("unpacking data")
     datalist = [[numpy.loadtxt(filename),label] for filename,label in zip(file_list, plot_labels)]
+    logger.info("reading cfg, assuming membrane/protein info identical across")
+    cfg.readfp(open(cfgs[0]))
+    protein_bottom = float(cfg.get('plotting','protein_bottom'))
+    protein_top = protein_bottom + float(cfg.get('plotting','protein_length'))
+    membrane_bottom = float(cfg.get('membrane','zmem'))
+    membrane_top = membrane_bottom + float(cfg.get('membrane','lmem'))
     logger.info("plotting")
+    fig = plt.figure()
+    axis = fig.add_subplot(111)
     for data,label in datalist:
-        plt.plot( data[:,2],data[:,3],label=label)
-    plt.legend()
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+        axis.plot( data[:,2],data[:,3],label=label)
+    axis.legend()
+    axis.set_title(title)
+    axis.set_xlabel(x_label)
+    axis.set_ylabel(y_label)
+    plot_bot,plot_top = axis.get_ylim()
+    protein_low,protein_high = [plot_bot*.6 + plot_top*.4,plot_bot*.4+plot_top*.6]
+    axis.fill_between([membrane_bottom,membrane_top],[plot_top,plot_top],[plot_bot,plot_bot],facecolor='yellow',alpha=0.3)
+    axis.fill_between([protein_bottom,protein_top],[protein_high,protein_high],[protein_low,protein_low],facecolor='red',alpha=0.3)
     logger.info("saving figure to {title}.png".format(title=title))
-    plt.savefig(title)
+    fig.savefig(title)
 
-def graph_mult_data_colors_cfgs(file_list,xcolumn,ycolumn,plot_labels,colors,x_label,ylabel,title,cfgs):
+def graph_mult_data_colors_cfgs(file_list,xcolumn,ycolumn,plot_labels,colors,x_label,y_label,title,cfgs):
     logger.info("unpacking data")
     datalist = [[numpy.loadtxt(filename),label,color] for filename,label,color in zip(file_list, plot_labels,colors)]
+    logger.info("reading cfg, assuming membrane/protein info identical across")
+    cfg.readfp(open(cfgs[0]))
+    protein_bottom = float(cfg.get('plotting','protein_bottom'))
+    protein_top = protein_bottom + float(cfg.get('plotting','protein_length'))
+    membrane_bottom = float(cfg.get('membrane','zmem'))
+    membrane_top = membrane_bottom + float(cfg.get('membrane','lmem'))
     logger.info("plotting")
+    fig = plt.figure()
+    axis = fig.add_subplot(111)
     for data,label,color in datalist:
-        plt.plot( data[:,xcolumn],data[:,ycolumn],label=label,color=color)
-    plt.legend()
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+        axis.plot( data[:,xcolumn],data[:,ycolumn],label=label,color=color)
+    axis.legend()
+    axis.set_title(title)
+    axis.set_xlabel(x_label)
+    axis.set_ylabel(y_label)
+    plot_bot,plot_top = axis.get_ylim()
+    protein_low,protein_high = [plot_bot*.6 + plot_top*.4,plot_bot*.4+plot_top*.6]
+    axis.fill_between([membrane_bottom,membrane_top],[plot_top,plot_top],[plot_bot,plot_bot],facecolor='yellow',alpha=0.3)
+    axis.fill_between([protein_bottom,protein_top],[protein_high,protein_high],[protein_low,protein_low],facecolor='red',alpha=0.3)
+
     logger.info("saving figure to {title}.png".format(title=title))
-    plt.savefig(title)
+    fig.savefig(title)
