c759082cbada4761d5296fffbb24d3588259d4da
Lennard van der Feltz <lvanderf@asu.edu>
2014-04-24 12:53:27 -0700

    Added fancier formatting to 1Dgraph.py

--- scripts/apbs-bornprofile-1Dgraph.py
+++ scripts/apbs-bornprofile-1Dgraph.py
@@ -17,7 +17,8 @@ Produces a matplotlib.pyplot plot of the data of the xcolumn vs the data of the 
 import bornprofiler
 import argparse
 import logging
-import pylab
+import matplotlib
+matplotlib.use('AGG')
 import sys
 from bornprofiler import plotting
 from bornprofiler.config import cfg
@@ -28,10 +29,11 @@ parser.add_argument('--xcolumn',default = 2)
 parser.add_argument('--cfgs', nargs = '+')
 parser.add_argument('--ycolumn',default = 3)
 parser.add_argument('--title', default = 'Born Energy')
-parser.add_argument('--xlabel',default = r'z $\AA$')
-parser.add_argument('--ylabel',default = 'kJ/mol')
+parser.add_argument('--xlabel',default = r'z ($\AA$)')
+parser.add_argument('--ylabel',default = r'$\mathcal{W}$_$\mathrm{elec}$ (kJ/mol)')
 parser.add_argument('--plot_labels',default=None, nargs = '+')
 parser.add_argument('--colors', default=None ,nargs = '+')
+parser.add_argument('--file_title', default = 'Born_Energy')
 args = parser.parse_args()
 dat_files = args.dat_files
 xcolumn = args.xcolumn
@@ -42,6 +44,7 @@ xlabel = args.xlabel
 ylabel = args.ylabel
 plot_labels = args.plot_labels
 colors = args.colors
+file_title = args.file_title
 bornprofiler.start_logging()
 # Checks to ensure matching numbers of labels, colors, and files.
 if  plot_labels == None or len(dat_files) == len(plot_labels):
@@ -60,15 +63,15 @@ else:
 if plot_labels == None:
     plot_labels = [dat_file.split('/')[-1].split('.')[0] for dat_file in dat_files]    
 # Follows automatic pylab color scheme if none specified. Additionally determines whether to apply cfg methods or not.
-if cfg ==None:
+if cfgs ==None:
     if colors == None:
-       plotting.graph_mult_data(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title)
+       plotting.graph_mult_data(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title,file_title)
     else:
-        plotting.graph_mult_data_colors(dat_files,xcolumn,ycolumn,plot_labels,colors,xlabel,ylabel,title) 
+        plotting.graph_mult_data_colors(dat_files,xcolumn,ycolumn,plot_labels,colors,xlabel,ylabel,title,file_title) 
 else:
     if colors == None:
-        plotting.graph_mult_data_cfgs(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title,cfgs)
+        plotting.graph_mult_data_cfgs(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title,file_title,cfgs)
     else:
-        plotting.graph_mult_data_colors_cfgs(dat_files,xcolumn,ycolumn,plot_labels,colors,xlabel,ylabel,title,cfgs)
+        plotting.graph_mult_data_colors_cfgs(dat_files,xcolumn,ycolumn,plot_labels,colors,xlabel,ylabel,title,file_title,cfgs)
 
 bornprofiler.stop_logging()   
