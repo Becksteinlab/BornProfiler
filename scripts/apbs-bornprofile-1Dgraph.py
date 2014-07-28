1be1e1cf0d5079d385f5507e1b0795064d14dbc3
Lennard van der Feltz <lvanderf@asu.edu>
2014-07-28 08:04:32 -0700

    Adapted 1dgraph.py to submit to a single function in bornprofiler/plotting.py with the option to use seaborn to improve plot quality.

--- scripts/apbs-bornprofile-1Dgraph.py
+++ scripts/apbs-bornprofile-1Dgraph.py
@@ -22,11 +22,13 @@ matplotlib.use('AGG')
 import sys
 from bornprofiler import plotting
 from bornprofiler.config import cfg
+
 logger = logging.getLogger("bornprofiler")
+
 parser = argparse.ArgumentParser()
 parser.add_argument('dat_files', nargs = '+')
 parser.add_argument('--xcolumn',default = 2)
-parser.add_argument('--cfgs', nargs = '+')
+parser.add_argument('--cfgs', nargs = '+',default=None)
 parser.add_argument('--ycolumn',default = 3)
 parser.add_argument('--title', default = 'Born Energy')
 parser.add_argument('--xlabel',default = r'z ($\AA$)')
@@ -34,6 +36,7 @@ parser.add_argument('--ylabel',default = r'$\mathcal{W}$$_\mathrm{elec}$ (kJ/mol
 parser.add_argument('--plot_labels',default=None, nargs = '+')
 parser.add_argument('--colors', default=None ,nargs = '+')
 parser.add_argument('--file_title', default = 'Born_Energy')
+parser.add_argument('--better_labels',action='store_true')
 args = parser.parse_args()
 dat_files = args.dat_files
 xcolumn = args.xcolumn
@@ -45,6 +48,7 @@ ylabel = args.ylabel
 plot_labels = args.plot_labels
 colors = args.colors
 file_title = args.file_title
+better_labels = args.better_labels
 bornprofiler.start_logging()
 # Checks to ensure matching numbers of labels, colors, and files.
 if  plot_labels == None or len(dat_files) == len(plot_labels):
@@ -56,22 +60,26 @@ if  plot_labels == None or len(dat_files) == len(plot_labels):
 else:
     logger.fatal("Number of plot labels does not match number of data files.")
     sys.exit(1)
-
-
+#Converts known ion names to custom labels which include charge and other information
+if better_labels:
+    better_labels = []
+    for item in plot_labels:
+        if item == 'Na':
+             better_labels.append(r"Na$^{+}$")
+        elif item == 'Cl':
+            better_labels.append(r"Cl$^{-}$")
+        elif item == 'K':
+            better_labels.append(r"K$^{+}$")
+        elif item == 'H':
+            better_labels.append(r"H$_{3}$O$^{+}$")
+        else:
+            better_labels.append(item)
+    plot_labels=better_labels   
 # If no labels are specified, generates them from the file names, removing directory and format information
 
 if plot_labels == None:
     plot_labels = [dat_file.split('/')[-1].split('.')[0] for dat_file in dat_files]    
 # Follows automatic pylab color scheme if none specified. Additionally determines whether to apply cfg methods or not.
-if cfgs ==None:
-    if colors == None:
-       plotting.graph_mult_data(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title,file_title)
-    else:
-        plotting.graph_mult_data_colors(dat_files,xcolumn,ycolumn,plot_labels,colors,xlabel,ylabel,title,file_title) 
-else:
-    if colors == None:
-        plotting.graph_mult_data_cfgs(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title,file_title,cfgs)
-    else:
-        plotting.graph_mult_data_colors_cfgs(dat_files,xcolumn,ycolumn,plot_labels,colors,xlabel,ylabel,title,file_title,cfgs)
+plotting.graph_mult_data(dat_files,xcolumn,ycolumn,plot_labels,xlabel,ylabel,title,file_title,colors=colors,cfgs=cfgs)
 
 bornprofiler.stop_logging()   
