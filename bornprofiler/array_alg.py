c2fe8429135140d953bdc6b4d2bc918a76bb8c06
Lennard van der Feltz <lvanderf@asu.edu>
2014-10-26 06:47:13 -0700

    Added ability to choose axis for operations in array_alg and error messages for when attempting unreasonable manipulations.

--- bornprofiler/array_alg.py
+++ bornprofiler/array_alg.py
@@ -5,49 +5,77 @@ from scipy.interpolate import interp1d
 class curve(object):
     """Class for encapsulating the information stored in a group of BP dat file.
     Allows for addition, subtraction, and averaging with appropriate 
-    interpolation."""
+    interpolation. Interpolation spacing and axis are provided as kwargs while
+    array containing [[x,y,z,E],...] is provided as first arg."""
     def __init__(self,*args,**kwargs):
         self.datarray = args[0]
+        self.numpoints = self.datarray.shape[0]
+        if self.numpoints < 2:
+            raise Exception("At least two points needed to define curve")
         self.spacial = self.datarray[:,0:3]
         self.values = numpy.array([self.datarray[:,-1]]).T
-        self.z = self.spacial[:,-1]
-        self.minz = numpy.amin(self.z)
-        self.maxz = numpy.amax(self.z)
-        self.zdensity = self.calc_zdensity()
+        try:
+            self.axis = kwargs['axis']
+        except:
+            self.axis = 2
+        self.axis_points = self.spacial[:,self.axis]
+        self.min_axis = numpy.amin(self.axis_points)
+        self.max_axis = numpy.amax(self.axis_points)
+        self.axis_density = self.calc_axis_density()
         try:
             self.spacing = kwargs['spacing']
         except:
-            self.spacing = 1/self.zdensity
+            self.spacing = 1/self.axis_density
 
-    def calc_zdensity(self):
-        distance = self.maxz - self.minz
-        density = (self.spacial.shape[0] - 1)/distance
+    def calc_axis_density(self):
+        distance = self.max_axis - self.min_axis
+        density = (float(self.numpoints) - 1)/distance
         return density
 
-    def z_interpolator(self):
-        return interp1d(self.z.flatten(),self.values.flatten())
+    def axis_interpolator(self):
+        return interp1d(self.axis_points.flatten(),self.values.flatten())
 
     def __add__(self,curve2):
+        if self.axis != curve2.axis:
+            raise Exception("Curves to be added do not have the same interpolation axis")
         spacing = max(self.spacing,curve2.spacing)
-        valid_range = [max(self.minz,curve2.minz),min(self.maxz,curve2.maxz)]
-        points = numpy.arange(valid_range[0],valid_range[1],spacing)
+        valid_min,valid_max = [max(self.min_axis,curve2.min_axis),min(self.max_axis,curve2.max_axis)]
+        if valid_min >= valid_max:
+            raise Exception("Curves to be added do not overlap on their interpolation axis")
+        points = numpy.arange(valid_min,valid_max,spacing)
         numpoints = points.shape[0]
-        result = self.z_interpolator()(points) + curve2.z_interpolator()(points)
-        spacial_array = numpy.hstack((numpy.zeros((numpoints,2)),points.reshape(numpoints,1)))
-        complete_array = numpy.hstack((spacial_array,result.reshape(numpoints,1)))
-        return curve(complete_array,spacing=spacing)
+        if numpoints < 2:
+            raise Exception("Curves to be added do not share sufficient space on interpolation axis to allow for addition")
+        result = self.axis_interpolator()(points) + curve2.axis_interpolator()(points)
+        output_array = numpy.zeros((numpoints,4))
+        output_array[:,self.axis] = points
+        output_array[:,3] = result 
+        return curve(output_array,spacing=spacing)
 
     def __sub__(self,curve2):
+        if self.axis != curve2.axis:
+            raise Exception("Curves to be subtracted do not have the same interpolation axis")
         spacing = max(self.spacing,curve2.spacing)
-        valid_range = [max(self.minz,curve2.minz),min(self.maxz,curve2.maxz)]
-        points = numpy.arange(valid_range[0],valid_range[1],spacing)
+        valid_min,valid_max = [max(self.min_axis,curve2.min_axis),min(self.max_axis,curve2.max_axis)]
+        if valid_min >= valid_max:
+            raise Exception("Curves to be subtracted do not overlap on their interpolation axis")
+        points = numpy.arange(valid_min,valid_max,spacing)
         numpoints = points.shape[0]
-        result = self.z_interpolator()(points) - curve2.z_interpolator()(points)
-        spacial_array = numpy.hstack((numpy.zeros((numpoints,2)),points.reshape(numpoints,1)))
-        complete_array = numpy.hstack((spacial_array,result.reshape(numpoints,1)))
-        return curve(complete_array,spacing=spacing)
+        if numpoints < 2:
+            raise Exception("Curves to be added do not share sufficient space on interpolation axis to allow for addition")
+        result = self.axis_interpolator()(points) - curve2.axis_interpolator()(points)
+        output_array = numpy.zeros((numpoints,4))
+        output_array[:,self.axis] = points
+        output_array[:,3] = result
+        return curve(output_array,spacing=spacing)
+
 
     def __div__(self,number):
         divided_vals = self.values/number
         complete_array = numpy.hstack((self.spacial,divided_vals))
+        return curve(complete_array)
+
+    def __mul__(self,number):
+        multiplied_vals = self.values * number
+        complete_array = numpy.hstack((self.spacial,multiplied_vals))
         return curve(complete_array)
