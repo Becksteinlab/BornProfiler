bd478739d7e97d704a6a3b49f860e82acc553a75
Oliver Beckstein <orbeckst@gmail.com>
2018-09-03 13:57:05 -0700

    added requirements.txt for ReadTheDocs

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -100,8 +100,6 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# -- Options for HTML output ---------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
